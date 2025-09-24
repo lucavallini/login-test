e7425b1730d4445a13afe0431af12a3d4815b21c
lucavallini <lucavallini1508@gmail.com>
2025-09-24 20:21:34 -0300

    24/09 20:21

--- conexion.py
+++ conexion.py
@@ -1,13 +1,13 @@
 import mysql.connector 
 import os
 
-with open ('passwordBDD.txt', 'r') as archivo:
+with open ('passwordBDD.txt', 'r') as archivo: #abro el archivo que tiene mi contraseña
     password = archivo.read()
 
 def getConexion():#Conexion a la base de datos.
     conexion= mysql.connector.connect(
         user= 'root',
-        password= password,
+        password= password, #lo pongo asi para cuando lo publique en github no lo vea todo el mundo
         host= 'localhost',
         database= 'registro_usuarios',
         port= 3306)
